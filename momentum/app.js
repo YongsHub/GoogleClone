3825de9702f17272d74cfa96609fa4f4174cce89
TAEYONG KIM <amico741@naver.com>
2022-01-02 14:11:47 +0900

    Update app.js

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,14 @@
-alert("hi");
+console.log(54545435);
+console.log("Hello");
+
+const a = 5;
+const b = 2;
+
+const myName = "taeyong";
+const veryLongVariableName =  "kimtaeyong";
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + myName);
+
+
