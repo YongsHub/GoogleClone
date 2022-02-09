60f5a477e5018c7879e54bb587443cc95ed1e22a
TAEYONG KIM <amico741@naver.com>
2022-02-10 02:02:29 +0900

    .

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # GoogleClone
 
 > HTML5 CSS, Javascript에 대한 전반적인 이해 HTML파일이 CSS파일과 Javascript파일을 가져오는 것임. 자바 스크립트 문법 ES6 공부
-> 
+
+- Javascript는 위에서 아래로 실행되는 것을 알자!
+
+### 📌 const 키워드와 let 키워드의 차이를 알자
+
+- const는 상수를 의미 즉, 값이 바뀌지 않음 let은 변수로 활용할 수 있다. 따라서 variable 값이 바뀌게 하고 싶지 않을때 const를 활용, 바뀌게 하고 싶을 때는 let 키워드를 활용하면 된다. (var은 쓰지 말 것!)
+
+* 많은 사람들이 const를 base로 쓰고 꼭 필요한 부분만 let을 이용한다.
--- momentum/app.js
+++ momentum/app.js
@@ -2,13 +2,55 @@ console.log(54545435);
 console.log("Hello");
 
 const a = 5;
-const b = 2;
+const b = 2; // const는 값을 바꾸지 않을 때 -> 상수 키워드를 의미한다.
 
-const myName = "taeyong";
+let myName = "taeyong";
 const veryLongVariableName =  "kimtaeyong";
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
-console.log("hello" + myName);
+console.log("hello" , myName);
+
+myName = "kim taeyong";
+
+console.log("my new name is " + myName);
+
+const amIFat = null; // 말 그대로 아무것도 없다는 뜻 아무것도 없는 상태 -> 의도적으로 아무것도 없다는 것을 표현
+const check = true;
+console.log(amIFat); // true or false 예를 들어 로그인, 웹사이트 로딩, 등등
+let something; //  undefined를 의미한다. variable은 존제하지만 undefined
 
 
+// 배열에 대해 알아보자 Array -> 하나의 variable에 list를 가질 수 있다.
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek[5]);
+
+// Add one more day to arrray
+
+daysOfWeek.push("sund"); // 요소 추가하는 방법
+console.log(daysOfWeek);
+
+// object 만들기
+
+
+const player = { // object에 접근할 때 : 활용 property를 추가하여 의미하는 바를 정해준다.
+    name : "nico",
+    points: 10,
+    fat: true,
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+player.fat = false;
+player.lastName = "Kim";
+console.log(player);
+
+function sayHello(){
+    console.log("Hello my name is taeyong");
+}
+
+sayHello();
