b39b44353a1649d59d2fee9659692ecab9b23329
TAEYONG KIM <amico741@naver.com>
2022-02-12 14:07:46 +0900

    .

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASS_NAME = "hidden";
 
 function onLoginSubmit(event) {
-    event.preventDefault(); // 브라우저의 기본 동작을 막아준다.
-    console.log(loginInput.value);
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASS_NAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASS_NAME);
+}
+
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event); // defaultPrevented : true를 확인할 수 있다.
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick); // JS에게 함수 이름만 주고 알아서 event가 발생할때 마다 실행해준다. 첫 번째 인자에는 여러 정보를 담아준다.
