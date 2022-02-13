a46c0f72231d29b0c6004af0d107975c04c5625b
TAEYONG KIM <amico741@naver.com>
2022-02-14 00:14:35 +0900

    .

---
+++ momentum/JS/background.js
@@ -0,0 +1,12 @@
+const images = ["1.jpeg", "2.jpeg", "3.jpeg"];
+
+// images 랜덤으로 가져오기
+const chosenImg = images[Math.floor(Math.random() * images.length)];
+// JS에서 img element 생성하기
+const bgImg = document.createElement('img');
+
+// 경로 설정
+bgImg.src = `img/${chosenImg}`;
+
+// JS로 img element body에 추가해주기
+document.body.prepend(bgImg); // prepend는 맨 앞에 오게 한다.
